bd7c2644722c8ee7b38ec927d5bd96500dceb07a
farom <farom@PC-FAROM-UBUNTU>
2016-12-20 22:09:58 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp indi_astroid_telescope /usr/bin/
+chmod +x /usr/bin/indi_astroid_telescope
+cp indi_astroid_simulator /usr/bin/
+chmod +x /usr/bin/indi_astroid_simulator
+cp indi_astroid.xml /usr/share/indi/
+cp IndiAstroidDriverPC.jar /usr/share/java
